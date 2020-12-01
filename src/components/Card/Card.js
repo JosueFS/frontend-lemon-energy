d61f40261a6fe238de16869dab96e0dc541711ee
JosueFS <josue.18rj@gmail.com>
2020-12-01 20:10:54 -0300

    style: adjust card footer position :art:

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -88,7 +88,7 @@ const Footer = styled(PartialContainer)(
 
       color: ${colors.text.caption};
     }
-
+    justify-content: flex-end;
     > *:not(:last-child) {
       margin-right: ${spacing(2)};
     }
